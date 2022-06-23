82068b24b6f173e4a3d21f787abe2a74ef47e14a
FrankFMY <pryanishnikovartem@gmail.com>
2022-06-23 17:43:44 +0300

    Step 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import './app.less';
 import {useDispatch, useSelector} from "react-redux";
 import {setCount} from "../reducers/reposReducer";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Main from './main/Main';
 
 const App = () => {
     const dispatch = useDispatch()
-    const count = useSelector(state => state.repos.count)
-
-    function onCountClick() {
-        dispatch(setCount(5))
-    }
 
     return (
-        <div className="app">
-            <button onClick={() => onCountClick()}>COUNT</button>
-            <div>{count}</div>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
---
+++ src/components/main/repo/Repo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './repo.less';
+
+const Repo = (props) => {
+    const repo = props.repo
+
+    return (
+        <div className="repo">
+            <div className="repo-header">
+                <div className="repo-header-name">{repo.name}</div>
+                <div className="repo-header-stars">Количество звёзд: {repo.stargazers_count}</div>
+            </div>
+            <div className="repo-last-commit">{repo.updated_at}</div>
+            <a href={repo.html_url} className="repo-link">Ссылка на репозиторий: {repo.html_url}</a>
+        </div>
+    );
+};
+
+export default Repo;
